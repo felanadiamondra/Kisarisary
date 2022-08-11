aa5fddbb9c6d616c09bd59ac9dc432130ad3c7e0
Diamondra RANDRIAMADISON <felana.diamondra.randriamadison@esti.mg>
2022-08-11 14:44:36 +0200

    set shape color

--- app/src/main/java/com/example/kisarisary/view/DrawingView.java
+++ app/src/main/java/com/example/kisarisary/view/DrawingView.java
@@ -71,6 +71,7 @@ public class DrawingView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        initializePaint();
         if(startX !=0 && startY !=0){
             if(shapeTmp.size() != 0){
                 paint.setColor(shapeTmp.get(0).getStrokeColor());
@@ -79,6 +80,7 @@ public class DrawingView extends View{
             }
 
             for(int i=0; i< shapes.size(); i++){
+                initializePaint();
                 switch (shapes.get(i).getType()){
                     case 1:
                         drawMeth = new DrawMethod(new DrawRect());
@@ -92,6 +94,7 @@ public class DrawingView extends View{
                 }
                 drawMeth.drawPaintImage(canvas, shapes.get(i), paint);
             }
+
         }
     }
 
@@ -104,7 +107,6 @@ public class DrawingView extends View{
 
     protected void drawTmpShape(ArrayList<Shape> shapeTmp, Canvas canvas){
         Shape shape = shapeTmp.get(0);
-        TouchCoordinates touchC = shape.getTouchC();
         switch (shape.getType()){
             case 1:
                 drawMeth = new DrawMethod(new DrawRect());
@@ -117,7 +119,6 @@ public class DrawingView extends View{
                 break;
         }
         drawMeth.drawPaintImageTmp(startX, startY, endX , endY, paint, canvas);
-        // canvas.drawRect(startX, startY, endX , endY, paint);
     }
 
     @Override
@@ -159,6 +160,9 @@ public class DrawingView extends View{
                 shapes.add(shape);
                 initializePaint();
                 invalidate();
+            }
+            else{
+                isPaintActive = false;
             }
         }
         return true;
